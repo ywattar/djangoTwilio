dd200036dbb745fe99c764b0a98a3232d5857cb7
yaser <ywattar@gmail.com>
2016-03-10 16:10:39 -0500

    trial2

--- djtwilio/views.py
+++ djtwilio/views.py
@@ -10,9 +10,9 @@ def gather_digits(request):
     twilio_response = Response()
 
     with twilio_response.gather(action='/respond/', numDigits=1) as g:
-        g.say('Press one to hear a song, two to receive an SMS')
+        g.say('Jawaaaaaaaaaaaaaaaaaaad, two to receive an SMS')
         g.pause(length=1)
-        g.say('Press one to hear a song, two to receive an SMS')
+        g.say('Jawwwaaaaaaaaaaaaaaaadd, two to receive an SMS')
 
     return twilio_response
 
