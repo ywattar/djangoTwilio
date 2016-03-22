7c179de705df326b095735ffc65e1ac4396a24cd
yaser <ywattar@gmail.com>
2016-03-21 20:07:49 -0400

    trial33

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts   import render
-from rdflib.plugins.sparql.operators import string
 
 
 def home (request):
@@ -15,6 +14,7 @@ def home (request):
 def delivered_receipt (request):
     print ("Hellllllllllllllooooooooooooo")
     if request.method == 'POST':
+        from rdflib.plugins.sparql.operators import string
         msgId= string (request.POST['phone_number'])
         print ('Message ID'+msgId)
         return HttpResponse("Message has delivered!")
